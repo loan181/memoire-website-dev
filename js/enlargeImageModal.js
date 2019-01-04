626be8ed25671f620d275c50d347d7c102d39f2b
loan181 <loan181@hotmail.fr>
2019-01-04 01:56:28 +0100

    add external link to image preview

--- js/enlargeImageModal.js
+++ js/enlargeImageModal.js
@@ -1,6 +1,7 @@
 
 $(function() {
     $('.pop').on('click', function() {
+        $('#imagepreviewLink').attr('href', $(this).attr('src'));
         $('.imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
         $('#imagemodal').modal('show');// imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
     });
