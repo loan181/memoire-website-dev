5826f11b7bcf9ac706c3eafdab66a9e266196898
loan181 <loan181@hotmail.fr>
2019-01-14 17:09:32 +0100

    add dynamix interaction with plot (using console)

---
+++ arbre/customJS/plotInteraction.js
@@ -0,0 +1,35 @@
+
+function resetAllDatas() {
+    for (let i = 0; i < 3; i++) {
+        figure.data[i].x = [];
+        figure.data[i].y = [];
+    }
+}
+
+function resetPlotWithDefaultData() {
+    resetAllDatas();
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 40; j++) {
+            let correspondingFlower = trainingSet[i*40 + j];
+            figure.data[i].x[j] = correspondingFlower.get("petal width");
+            figure.data[i].y[j] = correspondingFlower.get("petal length");
+        }
+    }
+    redrawPlot();
+}
+
+function drawPointsIndex(listOfMarkersIndex) {
+    resetAllDatas();
+    for (let i = 0; i < listOfMarkersIndex.length; i++) {
+        let markerIdx = listOfMarkersIndex[i];
+        let correspondingFlower = trainingSet[markerIdx];
+        let correspondingDataIdx =  Math.floor(markerIdx / 40);
+        figure.data[correspondingDataIdx].x.push(correspondingFlower.get("petal width"));
+        figure.data[correspondingDataIdx].y.push(correspondingFlower.get("petal length"));
+    }
+    redrawPlot();
+}
+
+function redrawPlot() {
+    Plotly.redraw("graphContainer");
+}
