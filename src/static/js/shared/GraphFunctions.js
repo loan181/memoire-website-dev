19bb94fd58c51f3f91a9c28ea7c9730ea604d72e
Loan <loan181@hotmail.fr>
2019-02-13 14:05:24 +0100

    update X and Y name on the modal

--- src/static/js/shared/GraphFunctions.js
+++ src/static/js/shared/GraphFunctions.js
@@ -5,11 +5,17 @@ var currentYCat = categories[1];
 window.onload = function() {
     $('#axisXChoice').change(function () {
         currentXCat = $(this).val();
+        try {
+            afterXChange();
+        } catch(e) {}
         refreshPlotInformation();
     });
 
     $('#axisYChoice').change(function () {
         currentYCat = $(this).val();
+        try {
+            afterYChange();
+        } catch(e) {}
         refreshPlotInformation();
     });
     addMarkersToCustomAdded();
