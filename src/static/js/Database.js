b4cd93228eea7396c49469a8fa993369800244a3
Loan <loan181@hotmail.fr>
2019-02-17 19:12:30 +0100

    log the user id (used by google analytics) to differentiate users

--- src/static/js/Database.js
+++ src/static/js/Database.js
@@ -6,6 +6,10 @@ function isLocalHost() {
     return window.location.hostname === "127.0.0.1" || window.location.hostname.includes("localhost");
 }
 
+function getGoogleAnalyticsClientID() {
+    return ga.getAll()[0].get('clientId');
+}
+
 function writeToDb(dicoInfo) {
     if (!isLocalHost()) {
         forceWriteToDb(dicoInfo)
@@ -15,6 +19,7 @@ function writeToDb(dicoInfo) {
 function forceWriteToDb(dicoInfo) {
     let logRef = db.ref('logs');
     dicoInfo["TS"] = firebase.database.ServerValue.TIMESTAMP; // Add the timestamp
+    dicoInfo["userID"] = getGoogleAnalyticsClientID(); // Add the user id
     logRef.push(dicoInfo);
 }
 
@@ -23,6 +28,7 @@ function logExercice(algo, exercice, answers, feedbacks) {
         let dicoInfo = {"algo":algo, "exercice":exercice, "answers":answers, "answers_feedbacks":feedbacks};
         let logRef = db.ref('exercices');
         dicoInfo["TS"] = firebase.database.ServerValue.TIMESTAMP; // Add the timestamp
+        dicoInfo["userID"] = getGoogleAnalyticsClientID(); // Add the user id
         logRef.push(dicoInfo);
     }
 }
