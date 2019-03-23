ba94a76cbd33ca639de1203d91eb01f3a150cb6d
loan181 <loan181@hotmail.fr>
2019-03-23 13:04:00 +0100

    correction on zoom on pinnable picture

--- src/static/js/enlargeImageModal.js
+++ src/static/js/enlargeImageModal.js
@@ -1,10 +1,14 @@
 
-$(function() {
-    $('.pop').on('click', function() {
-        $('#imagepreviewLink').attr('href', $(this).attr('src'));
-        $('.imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
-        $('#imagemodal').modal('show');// imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-    });
-});
 // Every img in the file is a pop up
 $("img").addClass("pop");
+$(function() {
+    $('img').on('click', imgOnClick);
+});
+
+function imgOnClick() {
+    console.log("trigger imgOnCLick");
+    $('#imagepreviewLink').attr('href', $(this).attr('src'));
+    $('.imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
+    $('#imagemodal').modal('show');// imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+}
+
