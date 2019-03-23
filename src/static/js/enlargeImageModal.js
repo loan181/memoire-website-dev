8b524e4270477353f32069c835308e5d55aa76f9
loan181 <loan181@hotmail.fr>
2019-03-23 13:04:45 +0100

    delete useless console.log

--- src/static/js/enlargeImageModal.js
+++ src/static/js/enlargeImageModal.js
@@ -6,7 +6,6 @@ $(function() {
 });
 
 function imgOnClick() {
-    console.log("trigger imgOnCLick");
     $('#imagepreviewLink').attr('href', $(this).attr('src'));
     $('.imagepreview').attr('src', $(this).attr('src')); // here asign the image to the modal when the user click the enlarge link
     $('#imagemodal').modal('show');// imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
