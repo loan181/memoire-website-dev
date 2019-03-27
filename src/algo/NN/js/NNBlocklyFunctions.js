3086e1f20432a122a2f818d1a97f7d563df1ad20
Loan <loan181@hotmail.fr>
2019-03-27 16:44:35 +0100

    add signature for NN blockly functions to do

---
+++ src/algo/NN/js/NNBlocklyFunctions.js
@@ -0,0 +1,53 @@
+// All this functions will be call by blockly blocks
+
+/**
+ * Return the maximum/minimum value of the last layer neuron values
+ *
+ * @param minOrMax either 'min' or 'max'
+ * @param nnOutputNeurons
+ */
+function nnSelectOutputLayer(minOrMax, nnOutputNeurons) {
+    // TODO
+    return;
+}
+
+
+/**
+ * Return the width of a picture.
+ * Return the width if picture is a 2D array
+ * Return the lenght if 1D
+ * @param picture the picture to return the width for
+ */
+function getPictureW(picture) {
+    //TODO
+    return;
+}
+
+/**
+ * Will flatten a picture.
+ * 2D picture is flatten into a 1D one
+ * @param pictureToFlatten
+ */
+function flattenPicture(pictureToFlatten) {
+    //TODO
+    return;
+}
+
+/**
+ * Set the value of the given neuron number with the given value
+ * @param value_neuron_ind
+ * @param value_picture_pixel
+ */
+function setValueOfNeuron(value_neuron_ind, value_picture_pixel) {
+    //TODO
+}
+
+/**
+ * Return the value of the pixelNumber of picture
+ * @param pixelNumber
+ * @param picture a 1D picture (array)
+ */
+function getValueOfImgPixel(pixelNumber, picture) {
+    //TODO
+    return;
+}
